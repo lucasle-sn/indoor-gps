a1c89a91d926143e9a091c2a7ae12f0d4d41d2c7
lucasle-sn <lqtrung1993@gmail.com>
2023-12-12 21:22:08 +1100

    Remove function moved to seperate lib

--- indoor_gps/helpers/getEulerAngle.m
+++
@@ -1,6 +0,0 @@
-%% Get Euler angles from Directional Cosine Matrix
-function [psi,theta,phi] = getEulerAngle(C)
-    theta = asind(C(2,3));
-    phi = atan2d(-C(1,3),C(3,3));
-    psi = atan2d(-C(2,1),C(2,2));
-end
