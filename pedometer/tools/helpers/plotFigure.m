3e3ffd7c56f829c6ad868b18c8936614628d2b58
lucasle-sn <lqtrung1993@gmail.com>
2023-12-12 21:22:51 +1100

    Continue to remove function moved to seperate lib

--- pedometer/tools/helpers/plotFigure.m
+++
@@ -1,8 +0,0 @@
-%% Figure
-% Plot Figure with given title, x label & y label
-function plotFigure(titleText, xlabelText, ylabelText)
-    figure; hold on; grid on;
-    title(titleText);
-    xlabel(xlabelText);
-    ylabel(ylabelText);
-end
